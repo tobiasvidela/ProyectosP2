417865afb161bc8cfd7f24b324b996fb4ccb100b
tobiasvidelaug14 <tobiasvidelaug14@gmail.com>
2024-08-27 14:55:10 -0300

    Adding Main.java class

---
+++ 00-RepasoJava/src/main/java/ejercicios_practicos/Main.java
@@ -0,0 +1,11 @@
+package ejercicios_practicos;
+
+/**
+ *
+ * @author Videla Guliotti Tobías Uriel
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" Ejercicios Prácticos - REPASO POO");
+    }
+}
